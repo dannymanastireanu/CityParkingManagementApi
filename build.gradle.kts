504fe52f9779fba4f5f00308d1c62d537f50906e
Dany Manastireanu <danym@microsoft.com>
2022-12-02 21:52:20 +0200

    Added config to run jar file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,42 @@
+import org.gradle.api.tasks.bundling.Jar
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     id("java")
+    application
+    id("org.springframework.boot") version "3.0.0"
 }
 
 group = "com.goosfraba"
 version = "1.0-SNAPSHOT"
+
+tasks.named<BootJar>("bootJar") {
+    archiveClassifier.set("boot")
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+val cpmMainClass = "com.goosfraba.Application"
+
+application {
+    mainClass.set(cpmMainClass)
+}
+
+springBoot {
+    mainClass.set(cpmMainClass)
+}
+
+tasks.named<BootJar>("bootJar") {
+    mainClass.set(cpmMainClass)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = cpmMainClass
+    }
+}
 
 repositories {
     mavenCentral()
@@ -14,6 +47,8 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.0")
     implementation("org.springframework.boot:spring-boot-autoconfigure:3.0.0")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-tomcat")
 }
 
 tasks.getByName<Test>("test") {
